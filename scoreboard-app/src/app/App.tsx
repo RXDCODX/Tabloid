323380d2128fa3e657bd48444bd69f69e5aed135
RXDCODX <maxxxes73rus@gmail.com>
2025-09-16 22:05:48 +0400

    Refactor scoreboard-app structure and remove unused files
    - Updated script reference in index.html to point to the new main.tsx location.
    - Updated asset references in dist/index.html to use new versions of JavaScript and CSS files.
    - Deleted unused CSS and JavaScript files, including App.css, App.tsx, index.css, and others, to streamline the codebase.
    - Removed unnecessary components and files to enhance project organization and maintainability.

---
+++ scoreboard-app/src/app/App.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AdminPanel from "../components/AdminPanel/AdminPanel";
+import styles from "./App.module.scss";
+import "../global.scss";
+import Scoreboard from "../components/Scoreboard/Scoreboard";
+import { SignalRContext } from "../providers/SignalRProvider";
+
+
+function App() {
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light",
+  );
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => setTheme((t) => (t === "dark" ? "light" : "dark"));
+
+  return (
+    <Router>
+      {typeof window !== "undefined" && window.innerWidth >= 768 && (
+        <div className={styles.navigationContainer}>
+          <div className={styles.navigationBar}>
+            <nav className={styles.navigation}>
+              <Link to="/scoreboard" className={styles.navLink}>
+                Scoreboard
+              </Link>
+              <Link to="/adminpanel" className={`${styles.navLink} ${styles.admin}`}>
+                AdminPanel
+              </Link>
+            </nav>
+          </div>
+        </div>
+      )}
+      <div className={styles.themeContainer}>
+        <div className={styles.themeButtonContainer}>
+          <button onClick={toggleTheme} className={styles.themeButton}>
+            {theme === "dark" ? "🌙 Тёмная" : "☀️ Светлая"}
+          </button>
+        </div>
+      </div>
+      <SignalRContext.Provider
+        value={{
+          connection: null,
+          automaticReconnect: true,
+          withCredentials: false,
+          url: "http://localhost:5035/scoreboardHub"
+        }}
+      >
+        <Routes>
+          <Route path="/scoreboard" element={<Scoreboard />} />
+          <Route path="/adminpanel" element={<AdminPanel />} />
+          <Route path="*" element={<Scoreboard />} />
+        </Routes>
+      </SignalRContext.Provider>
+    </Router>
+  );
+}
+
+export default App;
--- scoreboard-app/src/app/main.tsx
+++ scoreboard-app/src/app/main.tsx
@@ -2,6 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.module.scss";
 
 createRoot(document.getElementById("root")!).render(<App />);
